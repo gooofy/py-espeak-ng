c0ed44e01a998ec74b0bd56c3d1f2501e5f0a9d5
Guenter Bartsch <guenter@zamia.org>
2017-11-03 22:51:26 +0100

    preparing pypi release

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
-all:	README.html README
+all:	README.html README.md
 
 .PHONY:	tests clean dist
 
 %.html: %.adoc
 	asciidoctor -r asciidoctor-diagram -a toc $<
 
-
-README:	README.adoc
-		cp README.adoc README
+README.md: README.adoc
+	asciidoc -b docbook README.adoc
+	iconv -t utf-8 README.xml | pandoc -f docbook -t markdown_strict | iconv -f utf-8 > README.md
 
 tests:
 	nosetests
 
-dist:
+dist:	README.md
 	python setup.py sdist
 
 clean:
--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ from setuptools import setup, find_packages
 
 EXCLUDED = ['*.tests', '*.tests.*', 'tests.*', 'tests']
 
-setup(name         ='espeakng',
+setup(name         ='py-espeak-ng',
       version      ='0.1.0',
       description  ='Python interface for eSpeak NG',
+      url          ='https://github.com/gooofy/py-espeak-ng',
       classifiers  = [
                       'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
                       'Topic :: Multimedia :: Sound/Audio :: Speech',
                       'Operating System :: POSIX :: Linux',
                       'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
                      ],
+      keywords     = 'eSpeak NG TTS text to speech interface'
       platforms    = 'Linux',
       license      = 'LGPLv3',
       package_dir  = {'espeakng': 'espeakng'},
