1118e3ca00a6dc6efb2424612cef9129817d8ad0
Guenter Bartsch <guenter@zamia.org>
2018-03-17 13:08:50 +0100

    forgot to change license tag

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name         ='py-espeak-ng',
-      version      ='0.1.3',
+      version      ='0.1.4',
       description  ='Python interface for eSpeak NG',
       url          ='https://github.com/gooofy/py-espeak-ng',
       classifiers  = [
                       'Topic :: Multimedia :: Sound/Audio :: Sound Synthesis',
                       'Topic :: Multimedia :: Sound/Audio :: Speech',
                       'Operating System :: POSIX :: Linux',
-                      'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                      'License :: OSI Approved :: Apache Software License',
                       'Programming Language :: Python :: 2',
                       'Programming Language :: Python :: 2.7',
                       'Programming Language :: Python :: 3',
